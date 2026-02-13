b39e87d91b382dac2c83a30b77fd7f558d8631f9
Scott Lewis <scottslewis@gmail.com>
2026-02-12 16:20:42 -0800

    Added GroupsExtensionConfig with capabilities IDs

---
+++ org.openmcptools.extensions.groups/src/GroupsExtensionConfig.ts
@@ -0,0 +1,6 @@
+export class GroupsExtensionConfig {
+	public static EXTENSION_ID: string = "org.openmcptools/groups";
+	
+	public static SERVER_CAPABILITIES_ID: string = GroupsExtensionConfig.EXTENSION_ID + "/server";
+	public static CLIENT_CAPABILITIES_ID: string = GroupsExtensionConfig.EXTENSION_ID + "/client";
+}
